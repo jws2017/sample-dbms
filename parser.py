31dd55dec79e739e6942ed05a0530c84b76f2af1
jws2017 <34115792+jws2017@users.noreply.github.com>
2020-11-30 22:33:37 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,14 @@
-filename = "<insert random filename here>"
-"""Opening the file to be parsed"""
+import re
+
+actions = {"CREATE ": create_database, "CREATE TABLE": create_table, "CREATE INDEX": create_index, "USE": use, "SELECT": select}
+
 try:
-	file = open(filename, 'r')
-	 
+	file = open("<insert sql script here>.sql", 'r')
 except FileNotFoundError:
-	print("File not found)
+	print("File not found")
 else:
-	script = file.read()
-	commands = script.split(';')
+	for command in file.read().split(';'):
+		for test in actions.keys():
+			x = re.search(test, command)
 finally:
 	file.close()
-
-for command in commands:
-		tokens = command.split(" ")
-		for token in tokens:
-			parse(token)
-
-def parse(token: str) {
-	if token in keywords:
-}
