aa18a7f61cdd8fbaa3b9c771bf352b75b8f213f9
jws2017 <34115792+jws2017@users.noreply.github.com>
2020-12-07 08:58:43 -0500

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,37 +1,75 @@
 class Session:
+  
   def __init__(self):
     self.id = 0
     self.databases = []
     self.database_names = []
     self.current = None
+    
   def create_database(self, name):
-    if name in self.database_names: raise Exception("Database %s already exists".format(name))
-    db = Database(name)
-    self.databases.append(db)
-    self.database_names.append(name)
-    return db
+    if name in self.database_names:
+      print("Database %s already exists".format(name))
+    else:
+      db = Database(name)
+      self.databases.append(db)
+      self.database_names.append(name)
+      return db
+    
   def clear_current(self):
     if self.current is not None:
       self.current = None
+      print("The current active database has been cleared")
+    else:
+      print("There is no active database")
+      
   def set_current(self, db):
     if db not in self.databases:
-      raise Exception("Database %s does not exist".format(db.name))
+      print("Database %s does not exist".format(db.name))
     elif db == self.current:
       print("%s is already the current database".format(db.name))
     else:
       self.current = db
       print("%s is now the current database".format(db.name))
+      
+    def start_session(id=None):
+      if id is None:
+        return Session()
+      else:
+        pass #to implement restarting a session by its unique id
+    
+    def stop(self):
+      pass #to implement stopping current database session.
+    
+    def commit(self):
+      pass #to implement saving the current session to disk
+    
+    def rollback(self):
+      pass #to implement rollback
 
 class Database:
+  
   def __init__(self, name):
     self.name = name
+    self.tables = []
+    self.names = []
+    
   def create_table(name, column_dict):
-    pass
+    if name in self.names:
+      print("Table %s already exists".format(name))
+    else:
+      pass #to implement create_table
+      
   def drop_table(name):
-    pass
+    if name not in self.names:
+      print("Table " + name + " does not exist")
+    else:
+      pass #to implement drop_table
 
 class Table:
-  pass
+  
+  def __init__(self, name, columns):
+    self.name = name
+    self.columns = columns
 
 if __name__ == "__main__":
-  pass
+  pass #for testing and such
