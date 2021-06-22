f6bbb2f05bc4dd0efdcf455f94290736c7f90e83
jws2017 <34115792+jws2017@users.noreply.github.com>
2021-06-22 18:57:34 -0500

    Editing tokenizer and adding test cases

--- test_parser.py
+++ test_parser.py
@@ -2,6 +2,6 @@ import tokenizer
 
 if __name__ == "__main__":
     print(tokenizer.tokenize(""))
-    print(tokenizer.tokenize("CREATE TABLE table;"))
-    print(tokenizer.tokenize("CREATE assignment_database;"))
-    print(tokenizer.tokenize("CREATE dd"))
+    print(tokenizer.tokenize("CREATE TABLE thingy(col1 int, col2 string);"))
+    print(tokenizer.tokenize("CREATE DATABASE assignment_database;"))
+    print(tokenizer.tokenize("CREATE INDEX dd ON table(col1);"))
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,28 @@
+import re
+
+
 def tokenize(sql_command: str):
-	""""""
-	first_word = sql_command[:sql_command.find(" ")].casefold()
-	if first_word == "create":
-		return {"action": "create", "object_type": "database", "name": sql_command[len(first_word)+1:]}
+	"""
+	Takes a single statement and parses it for SQL. The syntax of the SQL commands that will be implemented are as follows:
+	statement := "CREATE DATABASE" identifier;
+	identifier := [A-Za-z_][A-Za-z0-9_]*"
+	"""
+	if sql_command.endswith(";"): sql_command = sql_command[:-1]
+	identifier = "[A-Za-z_][A-Za-z0-9]*"
+	type_ = identifier
+	create_database_pattern = f"CREATE DATABASE {identifier}"
+	create_table_pattern = f"CREATE TABLE {identifier}\W*\({identifier} {type_}(,\W*{identifier} {type_})*\)"
+	create_index_pattern = f"CREATE (UNIQUE )?INDEX {identifier} ON {identifier}\W*\({identifier}(,\W*{identifier})*\)"
+
+	if re.match(create_database_pattern, sql_command, re.IGNORECASE):
+		return {"action": "create", "object_type": "database", "name": sql_command[16:]}
+	if re.match(create_table_pattern, sql_command, re.IGNORECASE):
+		s = sql_command.find('(')
+		if s == -1: table_name = sql_command[13:]
+		else: table_name = sql_command[13:s]
+		return {"action": "create", "object_type": "table", "name": table_name}
+	if re.match(create_index_pattern, sql_command, re.IGNORECASE):
+		s = sql_command.casefold().find('on')
+		name = sql_command[13:s].strip()
+		table_name = sql_command[s+2:].strip()
+		return {"action": "create", "object_type": "index", "name": name, "on": table_name}
